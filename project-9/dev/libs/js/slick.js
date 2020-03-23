dce1d4716b70aed85e6a984b71e2aa9bf0a764aa
Andrei Positko <andrei.positko@chisw.com>
2020-03-23 18:32:50 +0200

    feedback slider

--- project-9/dev/libs/js/slick.js
+++ project-9/dev/libs/js/slick.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3
-});
-});
 
+  // nextArrow:
+	
+	// `<button type="button" class="slick-next">
+	// 	<img class="" src="img/feedback/arrow-next.png" alt="Фото отзыва">
+	// </button>`
+	
+});
+});
