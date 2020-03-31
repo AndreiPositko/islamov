18ae110c51bf2f8ee5724bf765a55ad10b36b845
Andrei Positko <andrei.positko@chisw.com>
2020-03-31 18:30:04 +0300

    adaptiv page

--- project-9/dev/libs/js/slick.js
+++ project-9/dev/libs/js/slick.js
@@ -2,8 +2,20 @@ $(document).ready(function(){
   $('.feedback-slider').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
-
+  slidesToScroll: 3,
+   responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    }
+   ]
+    
+    
   // nextArrow:
 	
 	// `<button type="button" class="slick-next">
